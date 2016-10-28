6a8ff81c46a48ac02c40005bd182e83aceb0235e
anishrijal <rijalanish@hotmail.com>
2016-10-28 14:39:37 -0500

    renamed the branch and authenticated all

--- server/api/employee/index.js
+++ server/api/employee/index.js
@@ -9,8 +9,8 @@ var router = new Router();
 
 router.get('/', auth.hasRole('admin'), controller.index);
 // router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/', auth.isAuthenticated(), controller.create);
 router.post('/:id', auth.isAuthenticated(), controller.update);
 
 module.exports = router;
