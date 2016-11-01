e63bf5f83f50bce74793fbd0696fc83c92db7450
anishrijal <rijalanish@hotmail.com>
2016-11-01 16:05:01 -0500

    deleted the error

--- client/app/employee/employee.controller.js
+++ client/app/employee/employee.controller.js
@@ -5,11 +5,6 @@
         .controller('EmployeeController', function (Employee) {
 
           this.employeeList = Employee.query();
-          // this.employeeList.$promise.then(()=>{
-          //   console.log(Employee.getOne(this.employeeList[0]._id));
 
-          });
-
-        })
-
+        });
     })();
--- client/app/employee/employee.service.js
+++ client/app/employee/employee.service.js
@@ -17,33 +17,7 @@
         }
       }
     });
-
-    // var resource = $resource('/api/employee/:id/:controller', {id: '@_id'},
-    //   {
-    //     create: {
-    //       method: 'post'
-    //     },
-    //     update: {
-    //       method: 'put'
-    //     },
-    //     getOne: {
-    //       method: 'get',
-    //       params: {
-    //         id: '@_id'
-    //       }
-    //     }
-    //   });
-  //   var employees = resource.query();
-  //
-  //     return {
-  //       getEmployee: employees,
-  //       getOne:function(id){
-  //         return resource.getOne({},{"_id":id})
-  //       }
-  //
-  //   }
-  // };
-
+  }
 
   angular.module('maerkApp')
     .factory('Employee', EmployeeResource);
