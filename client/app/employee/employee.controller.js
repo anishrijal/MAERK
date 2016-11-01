1a19b61fae95739b5de6e00f9160a05c399cff52
anishrijal <rijalanish@hotmail.com>
2016-11-01 15:42:48 -0500

    initial push for client side service

--- client/app/employee/employee.controller.js
+++ client/app/employee/employee.controller.js
@@ -1,128 +1,15 @@
-
 (function () {
-        'use strict';
+    'use strict';
 
-        angular.module('maerkApp')
-            .controller('EmployeeController', function ($scope, $mdToast) {
+    angular.module('maerkApp')
+        .controller('EmployeeController', function (Employee) {
 
-                    $scope.employeeList = [
-                      {
+          this.employeeList = Employee.query();
+          // this.employeeList.$promise.then(()=>{
+          //   console.log(Employee.getOne(this.employeeList[0]._id));
 
-                                     "name": "Adam Griffin",
-                                     "client": [
-                                       "Mudo"
-                                     ],
-                                     "skill": [
-                                       "DDA"
-                                     ],
-                                     "recruiter": "Arnold",
-                                     "revenue": 40000,
+          });
 
-                                   },
-                                   {
-                                     "name": "Patricia Bradley",
-                                     "client": [
-                                       "Flashdog",
-                                       "Dabtype",
-                                       "Topicshots"
-                                     ],
-                                     "skill": [
-                                       "Clinical Development"
-                                     ],
-                                     "recruiter": "Ricky",
-                                     "revenue": 40000,
+        })
 
-                                   }, {
-                                     "name": "Kathryn Ramos",
-                                     "client": [
-                                       "Kazio"
-                                     ],
-                                     "skill": [
-                                       "Screening"
-                                     ],
-                                     "recruiter": "Bobby",
-                                     "revenue": 40000,
-
-                                   }, {
-                                     "name": "Walter Diaz",
-                                     "client": [
-                                       "Skynoodle",
-                                       "Shufflester",
-                                       "Yodo"
-                                     ],
-                                     "skill": [
-                                       "JavaSE",
-                                       "Knowledge Sharing"
-                                     ],
-                                     "recruiter": "Ricky",
-                                     "revenue": 40000,
-
-                                   }, {
-                                     "name": "Clarence Coleman",
-                                     "client": [
-                                       "Latz",
-                                       "Wordify",
-                                       "Mycat"
-                                     ],
-                                     "skill": [
-                                       "SAP Business ByDesign",
-                                       "DVD Authoring"
-                                     ],
-                                     "recruiter": "Ricky",
-                                     "revenue": 40000,
-
-                                   }, {
-                                     "name": "Carolyn Cruz",
-                                     "client": [
-                                       "Browsedrive",
-                                       "Youtags",
-                                       "Divape"
-                                     ],
-                                     "skill": [
-                                       "OBIEE"
-                                     ],
-                                     "recruiter": "Arnold",
-                                     "revenue": 40000,
-
-                                   }, {
-                                     "name": "Timothy Ross",
-                                     "client": [
-                                       "Brightdog",
-                                       "Thoughtstorm"
-                                     ],
-                                     "skill": [
-                                       "Lawson HRIS"
-                                     ],
-                                     "recruiter": "Bobby",
-                                     "revenue": 40000,
-
-                                   }, {
-                                     "name": "Paul Fuller",
-                                     "client": [
-                                       "Voolith"
-                                     ],
-                                     "skill": [
-                                       "UCC filings"
-                                     ],
-                                     "recruiter": "Rocky",
-                                     "revenue": 40000,
-
-                                   }, {
-                                     "name": "Joe Walker",
-                                     "client": [
-                                       "Tavu",
-                                       "Gigaclub",
-                                       "Yodo"
-                                     ],
-                                     "skill": [
-                                       "Start-ups",
-                                       "NPO"
-                                     ],
-                                     "recruiter": "Ricky",
-                                     "revenue": 40000,
-
-                                   }
-
-                    ]
-                        });
-            }());
+    })();
