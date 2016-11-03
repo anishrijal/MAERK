8541194da5d93b48e6af97366e9e53f81c51d4fd
anishrijal <rijalanish@hotmail.com>
2016-11-03 09:49:27 -0500

    created the tools on employee table

--- client/app/employee/employee.controller.js
+++ client/app/employee/employee.controller.js
@@ -2,11 +2,51 @@
     'use strict';
 
     angular.module('maerkApp')
-        .controller('EmployeeController', function (Employee) {
+        .controller('EmployeeController', function (Employee, $mdToast) {
+              this.showOption = false;
+              this.showEdit = false;
+              this.showDelete = false;
+              var selected;
+              this.employeeList = Employee.query();
 
-          this.employeeList = Employee.query();
-          // this.employeeList.$promise.then(()=>{
-          //   console.log(Employee.getOne(this.employeeList[0]._id));
-          // });
-        });
+              this.selectedRowCallback = function(rows){
+                    selected = rows;
+                    if(selected.length==0){
+                      this.showOption = false;
+                      this.showEdit = false;
+                      this.showDelete = false;
+                    }
+                    else if(selected.length == 1){
+                      this.showOption = true;
+                      this.showDelete = true;
+                      this.showEdit =true;
+                    }
+                    else{
+                      this.showOption = false;
+                      this.showDelete = true;
+                      this.showEdit=false;
+                    }
+
+
+              }
+
+              // this.activate=function(){
+              //   alert('activate this employee');
+              // };
+              //
+              // this.deactivate=function(){
+              //   alert('deactivate this employee');
+              // };
+
+              this.edit = function(){
+                alert('inside edit');
+              };
+
+              this.delete = function(rows){
+
+                alert("are u sure u wanna delete");
+              }
+
+        })
+
     })();
