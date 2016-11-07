58a6cbd4fae0e88291562e6e8aa0d8b33002f698
anishrijal <rijalanish@hotmail.com>
2016-11-07 09:19:50 -0600

    adding the add

--- client/app/employee/employee.controller.js
+++ client/app/employee/employee.controller.js
@@ -2,12 +2,12 @@
     'use strict';
 
     angular.module('maerkApp')
-        .controller('EmployeeController', function (Employee, $mdToast) {
+        .controller('EmployeeController', function (Employee, $mdToast, $mdDialog) {
               this.showOption = false;
               this.showEdit = false;
               this.showDelete = false;
               var selected;
-              this.employeeList = Employee.query();
+              this.employeeList = Employee.getEmployee;
 
               this.selectedRowCallback = function(rows){
                     selected = rows;
@@ -28,18 +28,23 @@
                     }
 
 
-              }
+              };
 
-              // this.activate=function(){
-              //   alert('activate this employee');
-              // };
-              //
-              // this.deactivate=function(){
-              //   alert('deactivate this employee');
-              // };
+              this.edit = function(ev){
+                // this.editEmp = true;
+                // this.addEmp= false;
 
-              this.edit = function(){
-                alert('inside edit');
+                $mdDialog.show({
+
+                  controller: editController,
+                  controllerAs: 'vm',
+                  templateUrl: '/app/employee/employee.add.html',
+                  parent: angular.element(document.body),
+                  clickOutsideToClose:true,
+                  targetEvent: ev,
+                  fullscreen: this.customFullscreen
+                });
+
               };
 
               this.delete = function(rows){
@@ -47,6 +52,46 @@
                 alert("are u sure u wanna delete");
               }
 
-        })
+              this.add = function(ev) {
+                // this.editEmp = false;
+                // this.addEmp= true;
+
+                $mdDialog.show({
+                  controller: addController,
+                  controllerAs: 'vm',
+                  templateUrl: '/app/employee/employee.add.html',
+                  parent: angular.element(document.body),
+                  clickOutsideToClose:true,
+                  targetEvent: ev,
+                  fullscreen: this.customFullscreen
+
+                });
+
+              };
+
+              function addController(){
+                this.editEmp = false;
+                this.addEmp= true;
+                this.addEmployee = Employee.create;
+
+                this.cancel = function() {
+                      $mdDialog.cancel();
+              };
+
+            }
+              function editController(){
+
+                this.editEmp = true;
+                this.addEmp= false;
+                this.editEmployee = Employee.update;
+
+                this.cancel = function() {
+                      $mdDialog.cancel();
+              };
+
+            }
+
+        });
+
 
     })();
--- client/app/employee/employee.service.js
+++ client/app/employee/employee.service.js
@@ -2,24 +2,66 @@
 
 (function() {
 
-  function EmployeeResource($resource) {
-    return $resource('/api/employee/:id/:controller', {id: '@_id'},{
-      create: {
-        method: 'post'
-      },
-      update: {
-        method: 'put'
-      },
-      getOne: {
-        method: 'get',
-        params: {
-          id: '@_id'
-        }
-      }
+  function EmployeeResource($resource, $mdDialog) {
 
-      });
-    }
+    // return $resource('/api/employee/:id/:controller', {id: '@_id'},{
+    //   create: {
+    //     method: 'post'
+    //   },
+    //   update: {
+    //     method: 'put'
+    //   },
+    //   getOne: {
+    //     method: 'get',
+    //     params: {
+    //       id: '@_id'
+    //     }
+    //   }
+    //
+    //   });
+    // }
 
+    var resource = $resource('/api/employee/:id/:controller', {id: '@_id'}
+    ,
+         {
+           create: {
+             method: 'post'
+           },
+           update: {
+             method: 'put'
+           },
+           getOne: {
+             method: 'get',
+             params: {
+               id: '@_id'
+             }
+           }
+         });
+       var employees = resource.query();
+      //  var _employee = null;
+       return {
+           getEmployee: employees,
+           getOne:function(id){
+             return resource.getOne({},{"_id":id})
+           },
+           create: function(employee){
+
+             new resource(employee).$save().then(function (newEmployee){
+               employees.push(newEmployee);
+
+               $mdDialog.hide();
+
+             })
+
+           },
+           update: function(employee){
+            employee.$save().then(function(newEmployee){})
+
+
+          }
+       }
+
+     };
 
 
 
