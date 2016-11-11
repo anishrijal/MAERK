579aa703a00e7081d4a109c61d51de09a01b0a8c
anishrijal <rijalanish@hotmail.com>
2016-11-11 13:20:46 -0600

    activate/deactivate behaviour

--- client/app/employee/employee.controller.js
+++ client/app/employee/employee.controller.js
@@ -10,9 +10,14 @@
               var selected;
               this.employeeList = Employee.getEmployee;
 
+              console.log(this.employeeList.length);
+
 
               this.selectedRowCallback = function(rows){
                     selected = rows;
+
+
+
                     if(selected.length==0){
                       this.showOption = false;
                       this.showEdit = false;
@@ -20,19 +25,63 @@
                     }
                     else if(selected.length == 1){
                       this.showOption =true;
-                      this.showOptions = ["Activate" , "Deactivate"];
-                      this.activate =this.showOptions[0];
                       this.showDelete = true;
                       this.showEdit =true;
                     }
                     else{
-                      this.showOption = false;
+                      this.showOption = true;
                       this.showDelete = true;
                       this.showEdit=false;
                     }
 
 
               };
+
+              this.showOptions = [
+
+                {
+                  name: 'Acitvate',
+                  fn : ()=>{
+
+                    $mdDialog.show({
+                      controller: 'activateController',
+                      controllerAs: 'vm',
+                      template: '<div>Are you sure you want to activate<div>'+
+                                    '<md-button ng-click="vm.activate()">Yes</md-button>'+
+                                    '<md-button ng-click="vm.cancel()">No</md-button>',
+                      parent: angular.element(document.body),
+                      locals: {
+                        employeeList: this.employeeList,
+                        selected: selected
+                      },
+                      clickOutsideToClose:true
+                    })
+                  }
+
+                },
+                {
+                  name: 'Deactivate',
+                  fn : ()=>{
+                    $mdDialog.show({
+                      controller: 'deactivateController',
+                      controllerAs: 'vm',
+                      template: '<div>Are you sure you want to deactivate<div>'+
+                                    '<md-button ng-click="vm.deactivate()">Yes</md-button>'+
+                                    '<md-button ng-click="vm.cancel()">No</md-button>',
+                      parent: angular.element(document.body),
+                      locals: {
+                        employeeList: this.employeeList,
+                        selected: selected
+                      },
+                      clickOutsideToClose:true
+                    })
+                  }
+                }
+                ];
+
+              this.activate =this.showOptions[0].name;
+
+
 
 
 
