85ea78edac756df84172c3cc0bcaacda0ccea134
anishrijal <rijalanish@hotmail.com>
2016-11-16 10:12:46 -0600

    added oval chart

--- client/app/report/report.services.js
+++ client/app/report/report.services.js
@@ -22,32 +22,53 @@
              }
            }
          });
-
-
+      //
+      // var labels =[];
+      // var data =[];
       var reports={
         year:2015,
         january:[{
-
-            first_name: "Kathryn",
-            last_name: "Ramos",
-            skill: "Screening",
-            recruiter: "Bobby",
-            salary: 91679,
-            pay_vacation_cost: 6361
-            },
-            {
-            first_name: "Anish",
-            last_name: "Rijal ",
-            skill: "Nothing",
-            recruiter: "Bobby",
-            salary: 70000,
-            pay_vacation_cost: 6361
+          first_name: "Kathryn",
+          last_name: "Ramos",
+          client: "Tmobile",
+          skill: "JavaScript",
+          recruiter: "Bobby",
+          salary: 91679,
+          pay_vacation_cost: 6361
+          },
+          {
+          first_name: "Anish",
+          last_name: "Rijal ",
+          client: "Verison",
+          skill: "Css",
+          recruiter: "Bobby",
+          salary: 25000,
+          pay_vacation_cost: 6361
+          },
+          {
+          first_name: "Kanye",
+          last_name: "West",
+          client: "At&t",
+          skill: "HTML",
+          recruiter: "Bobby",
+          salary: 70090,
+          pay_vacation_cost: 6361
+          },
+          {
+          first_name: "Donald",
+          last_name: "Trump",
+          client: "At&t",
+          skill: "Node",
+          recruiter: "Bobby",
+          salary: 11090,
+          pay_vacation_cost: 6361
           }
-        ],
+      ],
         february:[{
           first_name: "Anish",
           last_name: "Rijal",
-          skill: "Nothing",
+          client: "At&t",
+          skill: "Csharp",
           recruiter: "Bobby",
           salary: 70000,
           pay_vacation_cost: 6361
@@ -55,6 +76,7 @@
           {
           first_name: "Donovan",
           last_name: "Perry",
+          client: "Verison",
           skill: "Java",
           recruiter: "Bobby",
           salary: 91890,
@@ -63,22 +85,33 @@
           {
           first_name: "Doan",
           last_name: "Per",
-          skill: "Java",
+          client: "Tmobile",
+          skill: "JavaScript",
           recruiter: "Bobby",
           salary: 91890,
           pay_vacation_cost: 6361
-          }
-
-        ]
-      }
+          }]
+      };
 
       //var reports = resource.query();
        return {
-
            getReport: function(month){
-            return reports[month]
+            return reports[month];
            },
-
+           getSkill: function(month){
+             var labels=[];
+            for(var i=0; i<reports[month].length; i++){
+              labels[i] =reports[month][i].skill;
+            }
+            return labels;
+           },
+           getSalary: function(month){
+             var data =[];
+            for(var i=0; i<reports[month].length; i++){
+              data[i] =reports[month][i].salary;
+            }
+            return data;
+          },
            getOne:function(id){
              return resource.getOne({},{"_id":id})
            },
